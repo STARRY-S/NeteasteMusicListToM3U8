73c9e1d8135911daf20ad4111fc4cff96e901de0
STARRY-S <hx916584160@gmail.com>
2019-09-28 15:59:27 +0800

    Version 1.1

--- README.md
+++ README.md
@@ -11,17 +11,26 @@ cp ./NeteasteMusicListToM3U8/main.py ~/Music && cd ~/Music
 python3 ./main.py
 ```
 
-After that, copy both `CloudMusic` directory and `M3U8` file into the root directory of your Walkman Music Player.
+It will generate a `txt` file and a `m3u8` file, you can use `-f/--file` option to load `txt` file if you want to regenerate the`M3U8`file.
+
+After that, checkout the `m3u8` file generated and correct the `Unable to find` file directory.
+
+The `ncm` file and "Unabel to find directory" file can't be play in Music player.
+
+Then, copy both `CloudMusic` directory and `M3U8` file into the root directory of your Walkman Music Player.
+
+Enjoy.
 
 ## Others:
 
 1. Make sure you've downloaded all music files in your music list.
 
-2. Replace `*.ncm` file to `mp3/flac` file first.
+2. Replace `*.ncm` file to `mp3/flac` file before execute this program.
 
 3. Make sure you have a good internet connection.
 
 # 简体中文：
+使用播放列表id导出网易云音乐的歌单为M3U8文件
 
 ## 使用：
 
@@ -34,7 +43,13 @@ cp ./NeteasteMusicListToM3U8/main.py ~/Music && cd ~/Music
 python3 ./main.py
 ```
 
-在此之后，复制生成的`.M3U8`文件和`CloudMusic`文件夹到你的Walkman播放器中。
+这将会生成一个`txt`文件和一个`m3u8`文件，如果你需要重新生成`m3u8`文件，你可以指定`-f/--file`参数来加载之前生成的`txt`文件。
+
+然后检查一下生成的M3U8文件是否正确，将里面`Unable to find`的文件目录替换为正确的文件目录。
+
+`ncm`文件和那些目录不正确的文件将无法被播放器识别播放。
+
+最后将生成的`m3u8`文件和`CloudMusic`文件夹一同复制到你的Walkman播放器/内存卡根目录中。
 
 ## 其他：
 
--- main.py
+++ main.py
@@ -1,34 +1,37 @@
-#!/bin/env python3
-#-*-coding:utf-8-*-
 import requests
 import os
+import sys
 
-list_id = input("Playlist ID: ")
+list_id = ''
 
 headers = {
 "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-"accept-encoding": "gzip, deflate, br",
 "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6",
-"cookie": "",
 "referer": "https://music.163.com/",
-"sec-fetch-mode": "nested-navigate",
-"sec-fetch-site": "same-origin",
-"upgrade-insecure-requests": "1",
 "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
 }
 
 def GetMusicList():
+    list_id = input("Playlist ID: ")
     list_url = "https://music.163.com/playlist?id=" + list_id
     req = requests.get(list_url, headers=headers)
     text = req.text.split('<li><a href="/song?id=')
     text[0] = text[0][text[0].find('title": "')+9:text[0].find('"images"')-3]
-    print("Music List Name:", text[0])
+    print("List Name:", text[0])
     for i in range(1, len(text)):
         song_id = text[i][0:text[i].find('">')]
         text[i]=text[i][text[i].find('">')+2:text[i].find('</a')]
         singer = GetSingerName(song_id)
         text[i] = singer + " - " + text[i]
+
+        # Edit Here To Replace Some Characters
+        text[i] = text[i].replace(' / ', ',')
+        text[i] = text[i].replace('&amp;', '&')
+        text[i] = text[i].replace('*', '＊')
+        text[i] = text[i].replace('/', '／')
+
         print(i, text[i])
+    ExportList(text, list_id)
     return text
 
 def GetSingerName(song_id):
@@ -48,14 +51,13 @@ def SearchFile(MusicName):
         for name in fnames:
             filename = os.path.join(fpath, name)
             filename = filename[2:]
-            if (MusicName in filename):
+            if MusicName in filename:
                 find = True
                 break
-        if (find):
+        if find:
             return filename
-
     print("Error: Unable to find file: ", MusicName)
-    return ""
+    return "Unable to find: " + MusicName
 
 def MakeM3U8(MusicList):
     list = "#EXTM3U\n"
@@ -66,12 +68,61 @@ def MakeM3U8(MusicList):
     filename = MusicList[0] + ".M3U8"
     file = open(filename, 'w')
     file.write(list)
-    file.close
+    file.close()
     print("Successfully write your music list into {}".format(filename))
 
+def ExportList(MusicList, list_id):
+    filename = list_id + '.txt'
+    file = open(filename, 'w')
+    text = ''
+    for i in range(0, len(MusicList)):
+        text = text + MusicList[i] + '\n'
+    file.write(text)
+    file.close()
+    print("\nExport music list txt file to {}".format(filename))
+
+def ReadList(filename):
+    try:
+        file = open(filename, 'rt')
+    except:
+        print("Error: File", filename, "Not Found!")
+        exit(0)
+    list = []
+    for line in file:
+        line = line.strip()
+        print(line)
+        list.append(line)
+    file.close()
+    return list
+
+def ShowHelp():
+    print("Convert Neteaste MusicList To M3U8 File\n")
+    print("Usage:\n\tpython3 main.py [options]\n")
+    print("Default: Get music list from internet and export it to txt file and m3u8 file")
+    print("\nGeneral Options:")
+    print("   -f, --file\t\tImport list from txt file which is exported from this program.")
+    print("   -v, --version\tShow Version.")
+    print("   -h, --help\t\tShow help.")
+
 def main():
-    print("Getting Music List info...")
-    MusicList = GetMusicList()
+    if len(sys.argv) == 1:
+        print("Getting Music List info from internet...")
+        MusicList = GetMusicList()
+    elif sys.argv[1] == '-f' or sys.argv[1] == '--file':
+        if len(sys.argv) == 2:
+            ShowHelp()
+            exit(0)
+        filename = sys.argv[2]
+        print("Getting Music List info from file:", filename)
+        MusicList = ReadList(filename)
+    elif sys.argv[1] == '-v' or sys.argv[1] == '--version':
+        print("Version: 1.1.0 - 2019.09.28")
+        print("By: STARRY-S")
+        exit(0)
+    else:
+        ShowHelp()
+        exit(0)
+
     MakeM3U8(MusicList)
 
 if __name__ == '__main__':
